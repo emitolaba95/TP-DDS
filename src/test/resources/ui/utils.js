ec7497c158ea1bf10befc9afef1429b9ea9ea133
emitolaba95 <emi.tolaba95@gmail.com>
2017-03-16 16:21:42 -0300

    asdasd

--- src/test/resources/ui/utils.js
+++ src/test/resources/ui/utils.js
@@ -1,3 +1,22 @@
+//vuelve a la pantalla anterior
 function goBack() {
-    window.history.back()
+    window.history.back();
+}
+
+//muestra una 'ventana emergente'
+function alertar(texto){
+    window.alert(texto);
+}
+
+function dar_bienvenida(){
+    alertar("Bienvenido!");
+}
+
+function verificar_pass(password){
+    pass = document.getElementById(password).innerHTML;
+    
+    if(pass.equals("")){
+        alertar("Debe ingresar una contraseña!");
+    }
+        
 }
