72402a1b2492bbdb100417e7601ffe86fdbfc942
emitolaba95 <emi.tolaba95@gmail.com>
2017-03-23 15:40:21 -0300

    responsive navbar

--- src/test/resources/ui/utils.js
+++ src/test/resources/ui/utils.js
@@ -16,8 +16,19 @@ function verificar_pass(){
     var falta_user = (user==="" || user===null) ;
     
     if(falta_pass || falta_user ){
-        alertar("Debe completar los datos!");
-      
+        alertar("Debe completar los datos!");      
     }
+    
+    //si falta alguna de los dos, esto da false y no se redirige la pagina
     return (!falta_pass && !falta_user );
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function ajustar_navBar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
